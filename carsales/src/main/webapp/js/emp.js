a3014d76c2e9b398246e4dff3f09d76b171bc5bf
Sathish <sathishprs@smartglossa>
2016-12-25 13:32:30 +0530

    change css file

--- carsales/src/main/webapp/js/emp.js
+++ carsales/src/main/webapp/js/emp.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
-	/*if (getCookie("uname") != undefined) {
+	if (getCookie("uname") != undefined) {
 		$("body")[0].appendChild(menu());
-		applyUserDetails();
-		getProfilePicture(getCookie("uname"));
-		$($(".mainArea")[0]).remove();
+	//	applyUserDetails();
+		//getProfilePicture(getCookie("uname"));
+		$($(".mainpage")[0]).remove();
 		var div = document.createElement("div");
-		div.className = "mainArea";
+		div.className = "mainpage";
 		$("body")[0].appendChild(div);
-		$(".mainArea")[0].appendChild(product());
-		$(".mainArea")[0].appendChild(displayProducts());	
+		$(".mainArea")[0].appendChild(cardetail());
+		//$(".mainArea")[0].appendChild(displayProducts());	
 	}
-});*/
+});
 
 $(document).on("click","#signup",function(key) {
 	var eid = $('#eId').val();
@@ -77,4 +77,43 @@ $(document).on("click","#signup",function(key) {
 				console.log(result);
 			});
 		});
+$(document).on("click","#login",function(key) {
+	var user = $('#user').val();
+	var passw = $('#passwo').val();
+
+	if (user == "") {
+		alert("Please Enter username ");
+		$("#user").focus().css("outline-color", "#ff0000");
+	}
+	if (passw == "") {
+		alert("Please Enter password");
+		$("#passw").focus().css("outline-color", "ff0000");
+		return;
+	}  //http://localhost:8080/carsales/car?operation=login&user=sathish&passwo=3232
+	var url = "/carsales/car?operation=login&user="
+			+ user + "&passwo=" + passw;
+	$.ajax({
+		url : url,
+		type : 'POST'
+	}).done(function(result) {
+		result = JSON.parse(result);
+		if (result.status == 1) {
+			document.cookie = "uname=" + user;
+			alert("succfully login");
+			$("body")[0].appendChild(menu());
+			//applyUserDetails();
+			//getProfilePicture(user);
+		$($(".mainpage")[0]).remove();
+			var div = document.createElement("div");
+			div.className = "mainpage";
+			$("body")[0].appendChild(div);
+			$(".mainArea")[0].appendChild(cardetail());
+			//$(".mainArea")[0].appendChild(displayProducts());	
+		} else {
+			alert("Error caused: " + result.message);
+		}
+
+	}).fail(function(result) {
+		console.log(result);
+	});
 });
