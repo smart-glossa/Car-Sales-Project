7e2fa37bd4afbf5324e85bb6b4e9b7088a89b8ef
Sathish <sathishprs@smartglossa>
2016-12-29 00:36:33 +0530

    contact file changes..

--- carsales/src/main/java/com/smartglossa/carsales/SalesConstant.java
+++ carsales/src/main/java/com/smartglossa/carsales/SalesConstant.java
@@ -2,9 +2,9 @@ package com.smartglossa.carsales;
 
 public class SalesConstant {
 	public static String MYSQL_DRIVER = "com.mysql.jdbc.Driver";
-	public static String DATABASE_NAME = System.getProperty("car.mysql.database");
-	public static String MYSQL_SERVER = System.getProperty("car.mysql.server");;
-	public static String USERNAME = System.getProperty("car.mysql.username");
-	public static String PASSWORD = System.getProperty("car.mysql.password");
+	public static String DATABASE_NAME ="carsales";                                              //System.getProperty("car.mysql.database");
+	public static String MYSQL_SERVER ="jdbc:mysql://localhost:3306";                                              //System.getProperty("car.mysql.server");;
+	public static String USERNAME = "root";                                                   //System.getProperty("car.mysql.username");
+	public static String PASSWORD ="root";                                                  //System.getProperty("car.mysql.password");
 
 }
